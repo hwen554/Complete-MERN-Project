b0be58216eef6593e60cff5e020dbe2b8510d8b8
Hong Weng <wengh7930@gmail.com>
2023-12-29 23:57:24 +1300

    update

--- foodi-client/src/components/Navbar.jsx
+++ foodi-client/src/components/Navbar.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from '/logo.png'
 import { BiPhoneCall } from 'react-icons/bi'
 const Navbar = () => {
+    const [isSticky, setSticky] = useState(false)
+    // handle scroll functions
+    useEffect(() => {
+        const handleScroll = () => {
+            const offset = window.scrollY;
+            if (offset > 0) {
+                setSticky(true);
+            } else {
+                setSticky(false);
+            }
+        };
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            console.log('successfully')
+            window.removeEventListener("scroll", handleScroll);
+            
+        };
+    }, [])
+
     const navItems = (
         <>
             <li>
-                <a href="/"> Home </a>
+                <a href="/" className="text-green"> Home </a>
             </li>
             <li tabIndex={0}>
                 <details>
@@ -31,8 +50,11 @@ const Navbar = () => {
         </>
     )
     return (
-        <header className="max-w-screen-2xl container mx-auto">
-            <div className="navbar xl:px-24">
+        <header className={`max-w-screen-2xl container mx-auto fixed top-0 left-0 right-0 transition-all duration-300 ease-in-out`}>
+            <div className={`navbar xl:px-24 ${isSticky
+                    ? "shadow-md bg-base-100 transition-all duration-300 ease-in-out"
+                    : ""
+                }`}>
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
