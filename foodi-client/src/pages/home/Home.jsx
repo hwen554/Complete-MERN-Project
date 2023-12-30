f402a11d1c2ef48a1085e5f0fdc9e05d92401fae
Hong Weng <wengh7930@gmail.com>
2023-12-30 22:23:20 +1300

    update

--- foodi-client/src/pages/home/Home.jsx
+++ foodi-client/src/pages/home/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import Banner from '../../components/Banner'
+import Catagories from './Catagories'
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Banner />
+      <Catagories />
+    </div>
   )
 }
 
