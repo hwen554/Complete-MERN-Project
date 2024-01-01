10ca3b6b1fc6d0199a6a8cd9050dfee32e9706be
Hong Weng <wengh7930@gmail.com>
2024-01-01 20:55:37 +1300

    update SpecialDishes

--- foodi-client/src/pages/home/SpecialDishes.jsx
+++ foodi-client/src/pages/home/SpecialDishes.jsx
@@ -1,12 +1,78 @@
 import React from 'react'
-
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import { FaAngleRight, FaAngleLeft  } from "react-icons/fa6";
 const SpecialDishes = () => {
+
+    const settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
   return (
     <div className="max-w-screen-2xl container mx-auto xl:px-24 px-4 my-20 relative">
           <div className='text-left'>
               <p className='subtitle'>Customer Favorites</p>
               <h2 className='title'>Popular Catagories</h2>
           </div>
+
+          <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+          <div>
+            <h3>7</h3>
+          </div>
+          <div>
+            <h3>8</h3>
+          </div>
+        </Slider>
+
 
           <div className="md:absolute right-3 top-8 mb-10 md:mr-24">
               <button onClick={() => slider?.current?.slickPrev()}
