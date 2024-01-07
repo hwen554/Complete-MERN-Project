007df93c752832ee6fdf44a708f6828b51a9a38e
Hong Weng <wengh7930@gmail.com>
2024-01-07 19:27:28 +1300

    update

--- foodi-client/src/layout/Main.jsx
+++ foodi-client/src/layout/Main.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import {Outlet} from 'react-router-dom'
 import Navbar from '../components/Navbar'
-import Banner from '../components/Banner'
-import Catagories from '../pages/home/Catagories'
+import Footer from '../components/Footer'
+
 const Main = () => {
   return (
     <div>
         <Navbar/>
-        {/* <Banner/> */}
         <Outlet/>
-        <footer>Footer</footer>
+        <Footer/>
     </div>
   )
 }
