74204b8a6442106016467bc54d6efc68a0575aee
Hong Weng <wengh7930@gmail.com>
2024-01-09 17:08:23 +1300

    Update Menu.jsx

--- foodi-client/src/pages/shop/Menu.jsx
+++ foodi-client/src/pages/shop/Menu.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import Cards from '../../components/Cards'
+import { FaFilter } from "react-icons/fa";
+
 
 const Menu = () => {
   return (
-    <div>Menu</div>
+    <div>
+        {/* menu banner */}
+        <div className='max-w-screen-2xl container mx-auto xl:px-24 px-4 bg-gradient-to-r from-0% from-[#FAFAFA] to-[#FCFCFC] to-100%'>
+            
+
+        </div>
+    </div>
   )
 }
 
