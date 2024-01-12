1991c581ea0fad8681a34b2b44a480a66db4c482
Hong Weng <wengh7930@gmail.com>
2024-01-12 18:52:28 +1300

    update

--- foodi-client/src/components/Navbar.jsx
+++ foodi-client/src/components/Navbar.jsx
@@ -105,18 +105,7 @@ const Navbar = () => {
                     </label>
                     {/*btn*/}
                     <button onClick={()=>document.getElementById('my_modal_5').showModal()} className="btn bg-green rounded-full px-6 text-white flex items-center gap-2"><FaRegUser />Login</button>
-                    <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
-                        <div className="modal-box">
-                            <h3 className="font-bold text-lg">Hello!</h3>
-                            <p className="py-4">Press ESC key or click the button below to close</p>
-                            <div className="modal-action">
-                                <form method="dialog">
-                                    {/* if there is a button in form, it will close the modal */}
-                                    <button className="btn">Close</button>
-                                </form>
-                            </div>
-                        </div>
-                    </dialog>
+                    <Modal/>
                 </div>
             </div>
         </header>
