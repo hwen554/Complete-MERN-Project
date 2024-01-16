480f2bb8860db11aa8ac091a05494b718db0707e
Hong Weng <wengh7930@gmail.com>
2024-01-16 15:31:52 +1300

    Create firebase.config.js

---
+++ foodi-client/src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC65Hr4iFqIrTsN9avCaALeQ4YxKCrRRZ4",
+  authDomain: "fir-foodi-client-bc58c.firebaseapp.com",
+  projectId: "fir-foodi-client-bc58c",
+  storageBucket: "fir-foodi-client-bc58c.appspot.com",
+  messagingSenderId: "435517212823",
+  appId: "1:435517212823:web:ebb8c13ad69e9856ac81d2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
