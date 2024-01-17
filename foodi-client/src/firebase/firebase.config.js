d28f1d1ce33ac2ebe950cdb96e2713479afbdd5f
Hong Weng <wengh7930@gmail.com>
2024-01-18 01:08:22 +1300

    Update firebase.config.js

--- foodi-client/src/firebase/firebase.config.js
+++ foodi-client/src/firebase/firebase.config.js
@@ -2,18 +2,18 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-console.log(import.meta.env.VITE_SOME_KEY) // 123
+// console.log(import.meta.env.VITE_SOME_KEY) 
 
 
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC65Hr4iFqIrTsN9avCaALeQ4YxKCrRRZ4",
-  authDomain: "fir-foodi-client-bc58c.firebaseapp.com",
-  projectId: "fir-foodi-client-bc58c",
-  storageBucket: "fir-foodi-client-bc58c.appspot.com",
-  messagingSenderId: "435517212823",
-  appId: "1:435517212823:web:ebb8c13ad69e9856ac81d2"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
