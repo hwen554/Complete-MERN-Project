16bd8cb7cfaec5048b311dfdcc44b06938e57e57
Hong Weng <wengh7930@gmail.com>
2024-01-18 21:10:12 +1300

    update

--- foodi-client/src/contexts/AuthProvider.jsx
+++ foodi-client/src/contexts/AuthProvider.jsx
@@ -1,14 +1,22 @@
 import React,{createContext,useState,useEffect } from 'react'
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth';
 import app from '../firebase/firebase.config';
-import axios from 'axios';
+// import axios from 'axios';
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({children}) => {
-
+    const authInfo = {
+        user, 
+        loading,
+        createUser, 
+        login, 
+        logOut,
+        signUpWithGmail,
+        updateUserProfile
+    }
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
     return (
