a2e65013c34cfcf35af10e93fec273394385fd7d
Hong Weng <wengh7930@gmail.com>
2024-01-21 13:40:13 +1300

    Update AuthProvider.jsx

--- foodi-client/src/contexts/AuthProvider.jsx
+++ foodi-client/src/contexts/AuthProvider.jsx
@@ -1,17 +1,29 @@
 import React,{createContext,useState,useEffect } from 'react'
-
+import {createUserWithEmailAndPassword, getAuth} from 'firebase/auth'
 // import axios from 'axios';
-
+import app from '../firebase/firebase.config'
+import { FaPassport } from 'react-icons/fa';
 export const AuthContext = createContext();
 
-
+const auth = getAuth(app) 
 const AuthProvider = ({children}) => {
-    const [user, setUser] = useState("");
+    const [user, setUser] = useState("Gavin");
+    const [Loading,setLoading] = useState(true);
+
+    //
+
+    const createUser = (email,password)=>{
+        return createUserWithEmailAndPassword(auth,email,password).
+        then((userCredential)=>{
+            //signup
+            const             
+        })
+    }
     const authInfo = {
         user
     }
     
-    const [loading, setLoading] = useState(true);
+    
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
