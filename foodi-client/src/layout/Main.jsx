5a17eb44a8f97faf30dce97190af6c0cb270004d
Hong Weng <wengh7930@gmail.com>
2024-02-07 11:41:35 +0800

    Update Main.jsx

--- foodi-client/src/layout/Main.jsx
+++ foodi-client/src/layout/Main.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {Outlet} from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import { AuthContext } from '../contexts/AuthProvider'
 
 const Main = () => {
+
+  const {loading} = useContext(AuthContext);
   return (
     <div>
       <Navbar />
